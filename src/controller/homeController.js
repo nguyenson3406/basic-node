fd4b31053ac674d61801fab7464fa4bd840c517a
nguyenson3406 <nguyenson3406@gmail.com>
2023-04-25 22:26:53 +0700

    CRUD

--- src/controller/homeController.js
+++ src/controller/homeController.js
@@ -1,26 +1,6 @@
-// import connection from "../configs/connetDB"
 import pool from "../configs/connetDB"
 
 let getHomepage = async (req,res) => {
-
-//     let data = [];
-//     connection.query(
-//         'SELECT * FROM `Info_user` ',
-//         function (err, results, fields) {
-//             results.map((row) => {
-//                 data.push({
-//                     id: row.id,
-//                     email: row.email,
-//                     address: row.address,
-//                     fistName: row.fistName,
-//                     lastName: row.lastName
-//                 })
-
-//             });
-
-//     return res.render('index.ejs', { dataUser: data })
-//     }
-// )
 
     const [rows, fields] = await pool.execute(`SELECT * FROM Info_user `);
     return res.render('index.ejs', {dataUser: rows})
@@ -36,6 +16,36 @@ let getCardpage = (req,res) => {
     return res.render('card.ejs')
 }
 
+let createUserpage = async (req,res) => {
+    let {fristName, lastName, email, address} = req.body;
+    const [user] = await pool.execute(`insert into info_user (email, address, fistName, lastName) 
+    values (?,?,?,?)`, [email, address, fristName, lastName]);
+
+    return res.redirect('/');
+}
+
+let getUpdatepage = async (req,res) => {
+    let userId = req.params.id;
+    const [user] = await pool.execute(`select * from info_user where id=?`, [userId]);
+    return res.render('update.ejs', {dataUser: user});
+}
+
+let postUpdateuser = async (req,res) => {
+    let userId = req.params.id;
+    let {fristName, lastName, email, address} = req.body;
+    await pool.execute(`update info_user 
+    set fistName = ?, lastName = ?, email = ?, address = ? where id=?`, 
+    [fristName, lastName, email, address, userId])
+    return res.redirect('/');
+}
+
+let postDeleteuser = async (req,res) => {
+    let userId = req.params.id;
+    await pool.execute(`delete from info_user where id = ?`, [userId]);
+    return res.redirect('/');
+}
+
 module.exports = {
-    getHomepage, getCardpage, getDetailpage
+    getHomepage, getCardpage, getDetailpage, createUserpage, getUpdatepage,
+    postUpdateuser, postDeleteuser
 }
--- src/route/web.js
+++ src/route/web.js
@@ -7,6 +7,10 @@ const initWebRoute = (app) => {
     route.get('/', homeController.getHomepage);
     route.get('/detail/:id', homeController.getDetailpage);
     route.get('/card', homeController.getCardpage);
+    route.post('/create-new-user', homeController.createUserpage);
+    route.get('/update-user/:id', homeController.getUpdatepage);
+    route.post('/update-user-DB/:id', homeController.postUpdateuser);
+    route.post('/delete/:id', homeController.postDeleteuser)
 
     return app.use('/',route)
 }
