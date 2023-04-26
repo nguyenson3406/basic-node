9870c2710589f1624c8dc241f870195606c9415c
nguyenson3406 <nguyenson3406@gmail.com>
2023-04-26 22:51:26 +0700

    middleware

--- src/server.js
+++ src/server.js
@@ -4,9 +4,18 @@ import initWebRoute from "./route/web"
 import initAPIRoute from "./route/api"
 
 require('dotenv').config();
+var morgan = require('morgan')
 
 const app = express();
 const port = process.env.PORT || 8080;
+
+app.use((req,res,next) => {
+    console.log('>>> run into my middleware')
+    console.log(req.method)
+    next();
+})
+
+app.use(morgan('combined'))
 
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
@@ -16,6 +25,10 @@ configViewEngine(app);
 initWebRoute(app);
 
 initAPIRoute(app);
+
+app.use((req, res) => {
+    return res.render('404.ejs')
+})
 
 app.listen(port,() => {
     console.log(`This is  http://localhost:${port}`)
